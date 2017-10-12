55becd564ae227ce06f309eee1f2ce99e0f06d6b
KingRaptor25 <rbndavid@hotmail.com>
2017-10-12 00:31:30 -0600

    Se modificaron la lista de post y se agregaron en base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-<<<<<<< HEAD
-# Create your views here.
-=======
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
->>>>>>> 31e8a87a52ff581219ef4b6ae0d8e3d7881a80f7
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts': posts})
